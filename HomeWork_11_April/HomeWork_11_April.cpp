6f4f554aac734a1a7d4fb9dfa68cabdd6f27b102
Glib Petrachkov <petrasha@gmail.com>
2022-04-17 21:02:28 +0300

    template

--- HomeWork_11_April/HomeWork_11_April.cpp
+++ HomeWork_11_April/HomeWork_11_April.cpp
@@ -208,3 +208,131 @@
 //	rev(n);
 //}
 
+// 1. Пузырьки
+//#include <iostream>
+//#include <Windows.h>
+//#include <time.h>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//template <typename T1>
+
+//void Bubble(T1 arr[], const int SIZE)
+//{
+//	int num = 0;
+//	for (int i = 0; i < SIZE - 1; i++)
+//	{
+//		for (int j = SIZE - 1; j > i; j--)
+//		{
+//			if (arr[j] < arr[j - 1])
+//			{
+//				num = arr[j - 1];
+//				arr[j - 1] = arr[j];
+//				arr[j] = num;
+//			}
+//		}
+//	}
+//}
+//
+//int main()
+//{
+//	srand(time(0));
+//	const int SIZE = 20;
+//	int arr[SIZE];
+//	for (int i = 0; i < SIZE; i++)
+//	{
+//		arr[i] = rand() % 21;
+//		cout << arr[i] << ", ";
+//	}
+//	cout << endl;
+//	Bubble(arr, SIZE);
+//	
+//	for (int i = 0; i < SIZE; i++)
+//	{
+//	cout << arr[i] << ", ";
+//	}
+//}
+
+//2. Insert
+//
+//#include <iostream>
+//#include <Windows.h>
+//#include <time.h>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//template <typename T1>
+//void Insert(T1 arr[], const int SIZE)
+//{
+//	T1 num = 0;
+//	for (int i = 0; i < SIZE; i++)
+//	{
+//		num = arr[i];
+//		for (int j = i - 1; j >= 0 && arr[j] > num; j--)
+//		{
+//			arr[j + 1] = arr[j];
+//			arr[j] = num;
+//		}
+//	}
+//}
+//
+//int main()
+//{
+//	srand(time(0));
+//
+//	const int SIZE = 20;
+//	int arr[SIZE];
+//
+//	for (int i = 0; i < SIZE; i++)
+//	{
+//		arr[i] = rand() % 26 + (-5);
+//		cout << arr[i] << ", ";
+//	}
+//
+//	Insert(arr, SIZE);
+//	cout << endl;
+//
+//	for (int i = 0; i < SIZE; i++)
+//	{
+//		cout << arr[i] << ", ";
+//	}
+//}
+
+//3. Choice
+
+#include<iostream>
+#include<Windows.h>
+#include<time.h>
+#include<algorithm>
+
+using namespace std;
+
+template <typename T1>
+
+void choice(T1 arr[], const int SIZE)
+{
+	sort(arr, arr + SIZE);
+}
+
+int main()
+{
+	srand(time(0));
+	const int SIZE = 20;
+	int arr[SIZE];
+	for (int i = 0; i < SIZE; i++)
+	{
+		arr[i] = rand() % 25 - 10;
+		cout << arr[i] << ", ";
+	}
+
+	choice(arr, SIZE);
+	cout << endl;
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << arr[i] << ", ";
+	}
+}
+
